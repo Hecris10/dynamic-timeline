e15f7199f90757c8667b0ba4aaa13eae277fdc9e
Helamã <helamaewerton@gmail.com>
2024-08-11 23:46:57 -0600

    refactor: Improve TimeLineItem component readability and add date formatting

--- src/components/TimeLineItem/index.tsx
+++ src/components/TimeLineItem/index.tsx
@@ -1,4 +1,5 @@
 import { useDrag } from "react-dnd";
+import { formatDate } from "../../lib/utils";
 import { TimelineItemData } from "../TimeLine";
 
 const TimelineItem = ({ item }: { item: TimelineItemData }) => {
@@ -10,11 +11,14 @@ const TimelineItem = ({ item }: { item: TimelineItemData }) => {
   return (
     <div
       ref={drag}
-      className="flex items-center justify-center bg-slate-300 shadow-lg rounded-lg cursor-grab p-3 box-border whitespace-nowrap text-ellipsis"
+      className="flex flex-col gap-1 items-center justify-center bg-slate-300 shadow-lg rounded-lg cursor-grab p-3 box-border whitespace-nowrap text-ellipsis overflow-hidden"
     >
-      <strong>{item.name}</strong>
-      <div>
-        {item.start} - {item.end}
+      <strong className="inline-block max-w-full overflow-hidden text-ellipsis">
+        {item.name}
+      </strong>
+      <div className="flex flex-wrap gap-1">
+        <p> {formatDate(item.start)}</p> <p className="text-center m-auto">-</p>{" "}
+        <p>{formatDate(item.end)}</p>
       </div>
     </div>
   );
